27121b20f196cbdb4805a7ad037ba20b983653d6
Joly Axel <joly.axel@outlook.fr>
2018-02-27 10:35:09 +0100

    First Commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Library;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Hello.Greet("Axel"));
+        }
+    }
+}
---
+++ Library/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Library
+{
+    public class Hello
+    {
+        public static string Greet(string name)
+        {
+            string message = "Hello " + name;
+            return message;
+        }
+        
+    }
+}
